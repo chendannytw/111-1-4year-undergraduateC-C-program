751ba4f776791e4caabc69f1e0ab781eb88eed23
1411131059 <113487682+1411131059@users.noreply.github.com>
2022-09-21 11:58:29 +0800

    Add files via upload

---
+++ 1411131059/text1.cpp
@@ -0,0 +1,42 @@
+﻿// text1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+	int u;
+	int a;
+	int t;
+
+	printf("Enter initial velocity\n");
+	scanf_s("%d", &u);
+
+	printf("Enter acceleration\n");
+	scanf_s("%d", &a);
+
+	printf("Enter the time\n");
+	scanf_s("%d", &t);
+
+	int finalvelocity;
+	finalvelocity = u + a * t;
+
+	int distancetraversed;
+	distancetraversed = u * t + (a * (t * t)) / 2;
+	
+	printf("final velocity is %d\n", finalvelocity);
+	printf("distance traversed is %d\n", distancetraversed);
+	
+}
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
