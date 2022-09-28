a1f61a0c43611e61bbb78352536da409c3270fe6
1411131059 <113487682+1411131059@users.noreply.github.com>
2022-09-28 11:53:32 +0800

    Add files via upload

---
+++ 1411131059/2.19.cpp
@@ -0,0 +1,75 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+    printf("Enter three different integers.");
+
+    int num1, num2, num3;
+
+    scanf_s("%d %d %d", &num1, &num2, &num3);
+
+    int sum;
+    sum = num1 + num2 + num3;
+
+    int average;
+    average = sum / 3;
+
+    int product;
+    product = num1 * num2 * num3;
+    
+    int small;
+    if (num1 < num2)
+    {
+        if (num1 < num3)
+        {
+            small = num1;
+        }
+        else
+        {
+            small = num3;
+        }
+    }
+    else
+    {
+        if (num2 < num3)
+        {
+            small = num2;
+        }
+        else
+        {
+            small = num3;
+        }
+    }
+    
+    int large;
+    if (num1 > num2)
+    {
+        if (num1 > num3)
+        {
+            large = num1;
+        }
+        else
+        {
+            large = num3;
+        }
+    }
+    else
+    {
+        if (num2 > num3)
+        {
+            large = num2;
+        }
+        else
+        {
+            large = num3;
+        }
+    }
+   
+    printf("Sum is %d\n", sum);
+    printf("Average is %d\n", average);
+    printf("Product is %d\n", product);
+    printf("Smallest is %d\n", small);
+    printf("Largest is %d\n", large);
+
+  }
+
