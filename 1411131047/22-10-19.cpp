9f415342176debb0d9460db544d055e167e47e3f
1411131047 <113487973+1411131047@users.noreply.github.com>
2022-10-26 08:54:58 +0800

    Update 22-10-19.cpp

--- 1411131047/22-10-19.cpp
+++ 1411131047/22-10-19.cpp
@@ -3,17 +3,19 @@
 #include <stdlib.h>
 
 int main() {
-    unsigned int i,temp,n;
-    double asw;
+    unsigned int i, temp, n,ed;
+    double asw,st;
 
     printf_s("Enter n: ");
     scanf_s("%d", &n);
     n = pow(10, n);
-    printf_s("%lf\n", s);
+    ed = n - 1;
+    st = (double)1 / n;
+    //printf_s("%lf\n", st);
 
     for (i = 1; i <= n; ++i) {
-        temp = rand() % n;
-        asw = (double)temp/n;
+        temp = rand() % ed;
+        asw = (double)temp / n + st;
         printf_s("%lf ", asw);
         if (i % 5 == 0) {
             printf_s("\n");
