2f8cc1a1e9adec5dd34fff4545f328d1fb7c23f5
s1411131053 <113761431+s1411131053@users.noreply.github.com>
2022-11-19 14:51:15 +0800

    Add files via upload

---
+++ 1411131053/11-4.cpp
@@ -0,0 +1,43 @@
+﻿// 11-4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+#define SIZE 10
+int main(void)
+{
+    int a[SIZE] = { 2,6,4,8,10,12,89,68,45,37 };
+    puts("Data iteam in original order");
+    int i;
+    for (i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+
+
+    for (unsigned int pass = 1; pass < SIZE; ++pass) {
+        for (size_t i = 0; i < SIZE - pass; ++i) {
+            if (a[i] > a[i + 1]) {
+                int hold = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = hold;
+            }
+        }
+    }
+
+    puts("\nData iteams in ascending order");
+
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d", a[i]);
+    }
+    puts("");
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
