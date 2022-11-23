898e2c390f4d81f4d3edd2c159c811a9d74f7894
s1411131053 <113761431+s1411131053@users.noreply.github.com>
2022-11-23 11:45:44 +0800

    Update 11-5.cpp

--- 1411131053/11-5.cpp
+++ 1411131053/11-5.cpp
@@ -46,6 +46,56 @@ int linearsearch(int id[], int sc[], int stu)
 
     return -1;
 }
+
+
+#include <stdio.h>
+#define SIZE 10
+//void sort(int a[], int c[]);
+int linearsearch(int id[], int sc[], int stu);
+int main(void)
+{
+    int a[SIZE] = { 2,6,4,8,10,12,89,68,45,37 };
+    int c[SIZE] = { 66,67,66,70,73,74,75,75,62,32 };
+    puts("Data iteam in original order");
+    int i, temp;
+    for (i = 0; i < SIZE; ++i) {
+        printf("\n");
+        printf("%4d", a[i]);
+        printf("%4d", c[i]);
+    }
+
+
+
+    //sort(a, c);
+    puts("\nData iteams in ascending order");
+    printf("Enter id:");
+    scanf_s("%d", &temp);
+    if (linearsearch(a, c, temp) <0) 
+        puts("not found");
+    else
+        printf("id=%d and score=%d", temp, c[linearsearch(a, c, temp)]);
+
+    
+   
+    
+    /*for (size_t i = 0; i < SIZE; ++i) {
+        printf("\n");
+        printf("%4d", a[i]);
+        printf("%4d", c[i]);
+    }
+    */
+
+    puts("");
+}
+
+int linearsearch(int id[], int sc[], int stu)
+{
+    
+    for (int i = 0; i < SIZE; i++)
+        if (stu == id[i]) return i;
+
+    return -1;
+}
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
 
