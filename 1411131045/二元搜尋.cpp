4e8ca32e4bcd142934638adeb5ecc1428331d188
1411131045 <113488050+1411131045@users.noreply.github.com>
2022-11-30 01:22:27 +0800

    Add files via upload

---
+++ 1411131045/二元搜尋.cpp
@@ -0,0 +1,101 @@
+﻿// 二元搜尋.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include <stdio.h>
+#define SIZE 20
+void sub1(int id[], int score[]);
+void search(int id[], int score[]);
+int binarysearch(const int b[], int key, int low, int high);
+int main(void)
+{
+	int id[SIZE] = { 12,13,14,15,16,17,19,21,22,23,24,26,27,28,29,31,32,33,36,37 };
+	int score[SIZE] = { 66,67,66,70,73,74,75,75,62,32,44,46,27,12,80,63,66,90,11,66 };
+	int ID = 0;
+	int key = 0;
+	int result = 0;
+	puts("Data items in original order");
+	for (size_t i = 0; i < SIZE; ++i)
+	{
+		printf("%4d", id[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < SIZE; ++i)
+	{
+		printf("%4d", score[i]);
+	}
+	sub1(id, score);
+	puts("\nData items in ascending order");
+	for (int i = 0; i < SIZE; ++i)
+	{
+		printf("%4d", id[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < SIZE; ++i)
+	{
+		printf("%4d", score[i]);
+	}
+	//search(id, score);
+	puts("");
+	printf("please enter the id:");
+	scanf_s("%d", &key);
+	if ((result = binarysearch(id, key, 0, SIZE - 1)) == -1)
+		printf("student not found!!");
+	else
+		printf("學號為%d的學生成績為:%d", key, score[result]);
+}
+void sub1(int id[], int score[])
+{
+	for (int pass = 1; pass < SIZE; ++pass)
+	{
+		for (int i = 0; i < SIZE - pass; ++i)
+		{
+			if (score[i] > score[i + 1])
+			{
+				int hold = score[i];
+				score[i] = score[i + 1];
+				score[i + 1] = hold;
+				int hold2 = id[i];
+				id[i] = id[i + 1];
+				id[i + 1] = hold2;
+			}
+		}
+	}
+}
+/*void search(int id[], int score[])
+{
+	int ID, j = 0;
+	printf("\nenter student id:");
+	scanf_s("%d", &ID);
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (ID == id[i])
+			printf("Student %d score is %d!", id[i], score[i]);
+		else
+			j++;
+	}
+	if (j > SIZE-1)
+		printf("Student %d not found!", ID);
+}*/
+int binarysearch(const int b[], int key, int low, int high)
+{
+	while (low <= high)
+	{
+		int middle = (low + high) / 2;
+		if (key == b[middle])
+			return middle;
+		else if (key < b[middle])
+			high = middle - 1;
+		else
+			low = middle + 1;
+	}
+	return -1;
+}
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
---
+++ 1411131045/二維陣列.cpp
@@ -0,0 +1,82 @@
+﻿// 二維陣列.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+#define STUDENTS 3
+#define EXAMS 4
+int min(const int grades[][EXAMS],int pupils,int tests);
+int max(const int grades[][EXAMS],int pupils,int tests);
+double average(const int setofgrades[], int tests);
+void printarray(const int grades[][EXAMS], int pupils, int tests);
+int main(void)
+{
+    int studentgrades[STUDENTS][EXAMS] = { {77,68,86,73},{96,87,89,78}, {70,90,86,81} };
+    puts("The array is:");
+    printarray(studentgrades,STUDENTS,EXAMS);
+    printf("\n\nLowest grade:%d\nHighest grade:%d\n",
+        min(studentgrades, STUDENTS, EXAMS),
+        max(studentgrades, STUDENTS, EXAMS));
+    for (int i = 0; i < STUDENTS; i++)
+    {
+        printf("The average grade for student %d is %.2f\n",i,average(studentgrades[i],EXAMS));
+    }
+}
+int min(const int grades[][EXAMS], int pupils, int tests)
+{
+    int lowgrade = 100;
+    for (int i = 0; i < pupils; i++)
+    {
+        for(int j=0;j<tests;++j)
+        {
+            if (grades[i][j] < lowgrade)
+                lowgrade = grades[i][j];
+        }
+    }
+    return lowgrade;
+}
+int max(const int grades[][EXAMS], int pupils, int tests)
+{
+    int highgrade = 0;
+    for (int i = 0; i < pupils; i++)
+    {
+        for (int j = 0; j < tests; ++j)
+        {
+            if (grades[i][j] > highgrade)
+                highgrade = grades[i][j];
+        }
+    }
+    return highgrade;
+}
+double average(const int setofgrades[], int tests)
+{
+    int total = 0;
+    for (int i = 0; i < tests; i++)
+    {
+        total += setofgrades[i];
+    }
+    return (double)total / tests;
+}
+void printarray(const int grades[][EXAMS], int pupils, int tests)
+{
+    printf("%s", "           [0] [1] [2] [3]");
+    for (int i = 0; i < pupils; i++)
+    {
+        printf("\nstudentgrades[%d]", i);
+        for (int j = 0; j <tests ; j++)
+        {
+            printf("%-5d", grades[i][j]);
+        }
+    }
+}
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
