63bd86f58855f4252ecff936ebc6618e8efc9c4c
s1411131053 <113761431+s1411131053@users.noreply.github.com>
2022-12-05 11:51:10 +0800

    Update 13.cpp

--- 1411131053/13.cpp
+++ 1411131053/13.cpp
@@ -18,6 +18,92 @@ int main(void)
         "each other\n&*aPtr = %p"
         "\n*&aPtr = %p\n", &*aPtr, *&aPtr);
 }
+=================================================================
+    #include <stdio.h>
+
+int cubeByValue(int n);
+
+int main(void)
+{
+    int number = 5;
+
+    printf("The original value of number is %d", number);
+
+    number = cubeByValue(number);
+
+    printf("\nThe new value of number is %d\n", number);
+}
+int cubeByValue(int n)
+{
+    return n * n * n;
+}
+====================================================
+    #include <stdio.h>
+
+void cubeByReference(int *nPtr);
+
+int main(void)
+{
+    int number = 5;
+
+    printf("The original value of number is %d", number);
+
+    cubeByReference(&number);
+
+    printf("\nThe new value of number is %d\n", number);
+}
+void cubeByReference(int *nPtr)
+{
+    *nPtr = *nPtr * *nPtr * *nPtr;
+}
+=============================================================
+    #include <stdio.h>
+#define SIZE 10
+
+void bubbleSort(int * const array, const size_t size);
+
+int main(void)
+{
+    int a[SIZE] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+
+    puts("Data items in original order");
+
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d",a[i]);
+    }
+
+    bubbleSort(a, SIZE);
+
+    puts("\nData items in ascending order");
+
+    for (size_t i = 0; i < SIZE; ++i) {
+        printf("%4d",a[i]);
+    }
+    puts("");
+}
+
+void bubbleSort(int* const array, const size_t size)
+{
+    void swap(int *element1Ptr, int * element2Ptr);
+
+    for (unsigned int pass = 0; pass < size - 1; ++pass) {
+        for (size_t j = 0; j < size - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j+1]);
+            }
+        }
+    }
+}
+
+void swap(int* element1Ptr, int* element2Ptr)
+{
+    int hold = *element1Ptr;
+    *element1Ptr = *element2Ptr;
+    *element2Ptr = hold;
+}
+=================================================================
+  
+    
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
