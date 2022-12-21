eb70003cb26651c74c2d4614bf82fdc76de2591d
s1411131053 <113761431+s1411131053@users.noreply.github.com>
2022-12-21 10:46:54 +0800

    Update 15.cpp

--- 1411131053/15.cpp
+++ 1411131053/15.cpp
@@ -1,76 +1,107 @@
-﻿// 15.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define SUITS 4
-#define FACES 13
-#define CARDS 52
-
-
-void shuffle(unsigned int wDeck[][FACES]);
-void deal(unsigned int wDeck[][FACES], const char *wFace[],const char *wSuit[]);
-
-
+#define SIZE 4
+#define stu 10
+//int binarySearch(int b[], int searchKey, int low, int high);
+void sort(const char** name, int a[][SIZE], int size);
+void swap(int* e1Ptr, int* e2Ptr);
+// function main begins program execution
 int main(void)
 {
-    unsigned int deck[SUITS][FACES] = {0};
-
-    srand(time(NULL));
-    shuffle(deck);
-
+    int key = 0, result = 0, i, j;
+    // initialize a
+    int a[stu][SIZE] = { {2,98,72,0},
+                        {3,76,32,0},
+                        {6,98,67,0},
+                        {7,89,32,0},
+                        {8,98,55,0},
+                        {9,89,32,0},
+                        {10,98,65,0},
+                        {11,99,32,0},
+                        {12,98,71,0},
+                        {13,76,91,0}};
     
-
-    const char *suit[SUITS] =
-    {"Hearts", "Diamonds", "Clubs", "Spades"};
-
-
-    const char *face[FACES] =
-    {"Ace", "Deuce", "Three", "Four",
-     "Five", "Six", "Seven", "Eight",
-     "Nine", "Ten", "Jack", "Queen", "King"};
-
-    deal(deck, face, suit);
-}
-
-
-void shuffle(unsigned int wDeck[][FACES])
-{
-
-    for (size_t card = 1; card <= CARDS; ++card) {
-        size_t row;
-        size_t column;
-
-
-        do {
-            row = rand() % SUITS;
-            column = rand() % FACES;
-        }while(wDeck[row][column] != 0);
-
-
-        wDeck[row][column] = card;
+const char *name[stu] =
+     { "How哥",
+        "阿明",
+        "小美",
+        "阿強",
+        "啾啾鞋",
+        "魔鞋啾啾",
+        "啾啾鞋魔",
+        "啾鞋魔啾",
+        "陳孜昊",
+        "謝任杰"};
+    puts("Data items in original order");
+    // output original array
+    for (j = 0; j < stu; j++)
+    {
+        for (i = 1; i < SIZE - 1; ++i) {
+            a[j][3] += a[j][i];
+        }
     }
+
+    for (j = 0; j < stu; j++) {
+    
+        printf("%8s", name[j]);
+        for (i = 0; i < SIZE; ++i) {
+            printf("%4d", a[j][i]);
+        }
+        printf("\n");
+    }
+
+    /*    printf("\n");
+        for (i = 0; i < SIZE; ++i) {
+            printf("%4d", a[1][i]);
+        }
+    */
+    // bubble sort                                         
+    // loop to control number of passes                    
+
+    sort(name, a, stu);
+    puts("\nData items in ascending order");
+
+    for (j = 0; j < stu; j++)
+    {
+        printf("%8s", name[j]);
+        for (i = 0; i < SIZE; ++i) {
+            printf("%4d", a[j][i]);
+        }
+        printf("\n");
+    }
+
 }
 
 
-void deal(unsigned int wDeck[][FACES], const char* wFace[],
-    const char* wSuit[])
-{
+void sort(const char **name, int a[][SIZE], int size)
+{const char *ptr;
+    int hold, pass, i;
+    for (pass = 1; pass < size; ++pass) {
 
-    for (size_t card = 1; card <= CARDS; ++card) {
+        // loop to control number of comparisons per pass   
+        for (i = 0; i < size - pass; ++i) {
 
-        for (size_t row = 1; row < SUITS; ++row) {
-
-            for (size_t column = 0; column < FACES; ++column) {
-
-                if (wDeck[row][column] == card) {
-                    printf("%5s of %-8s%c", wFace[column], wSuit[row],
-                        card % 2 == 0 ? '\n' : '\t');
-                }
+            // compare adjacent elements and swap them if first 
+            // element is greater than second element           
+            if (a[i][3] < a[i + 1][3]) {
+                ptr=name[i];
+                name[i]=name[i+1];
+                name[i+1]=ptr;
+                swap(&a[i][0], &a[i + 1][0]);
+                swap(&a[i][1], &a[i + 1][1]);
+                swap(&a[i][2], &a[i + 1][2]);
+                swap(&a[i][3], &a[i + 1][3]);
             }
         }
     }
+
+}
+
+void swap(int* e1Ptr, int* e2Ptr)
+{
+    int hold;
+    hold = *e1Ptr;
+    *e1Ptr = *e2Ptr;
+    *e2Ptr = hold;
 }
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
