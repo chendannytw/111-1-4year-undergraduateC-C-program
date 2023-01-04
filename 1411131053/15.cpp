1933d22dad673920d15dcaea93af48d5930f018a
s1411131053 <113761431+s1411131053@users.noreply.github.com>
2023-01-04 10:13:42 +0800

    Update 15.cpp

--- 1411131053/15.cpp
+++ 1411131053/15.cpp
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #define SIZE 4
 #define stu 10
-//int binarySearch(int b[], int searchKey, int low, int high);
 void sort(const char** name, int a[][SIZE], int size);
 void swap(int* e1Ptr, int* e2Ptr);
-int minimum(const int grades[][SIZE], size_t pupils, size_t tests);
-int maximum(const int grades[][SIZE], size_t pupils, size_t tests);
-double average(const int setOfGrades[], size_t tests);
-void printArray(const int grades[][SIZE], size_t pupils, size_t tests);
-// function main begins program execution
+
 int main(void)
 {
     int key = 0, result = 0, i, j;
-    // initialize a
+    
     int a[stu][SIZE] = { {2,98,72,0},
                         {3,76,32,0},
                         {6,98,67,0},
@@ -22,24 +17,24 @@ int main(void)
                         {10,98,65,0},
                         {11,99,32,0},
                         {12,98,71,0},
-                        {13,76,91,0}};
-    
-const char *name[stu] =
-     { "How哥",
-        "阿明",
-        "小美",
-        "藍亦明",
-        "啾啾鞋",
-        "魔鞋啾啾",
-        "啾啾鞋魔",
-        "啾鞋魔啾",
-        "陳孜昊",
-        "謝任杰"};
+                        {13,76,91,0} };
+
+    const char* name[stu] =
+    { "How哥",
+       "阿明",
+       "小美",
+       "藍亦明",
+       "啾啾鞋",
+       "魔鞋啾啾",
+       "啾啾鞋魔",
+       "啾鞋魔啾",
+       "陳孜昊",
+       "謝任杰" };
 
 
 
     puts("Data items in original order");
-    // output original array
+    
     for (j = 0; j < stu; j++)
     {
         for (i = 1; i < SIZE - 1; ++i) {
@@ -48,7 +43,7 @@ const char *name[stu] =
     }
 
     for (j = 0; j < stu; j++) {
-    
+
         printf("%8s", name[j]);
         for (i = 0; i < SIZE; ++i) {
             printf("%4d", a[j][i]);
@@ -61,8 +56,7 @@ const char *name[stu] =
             printf("%4d", a[1][i]);
         }
     */
-    // bubble sort                                         
-    // loop to control number of passes                    
+                    
 
     sort(name, a, stu);
     puts("\nData items in ascending order");
@@ -79,35 +73,26 @@ const char *name[stu] =
 }
 
 
-void sort(const char **name, int a[][SIZE], int size)
-{const char *ptr;
+void sort(const char** name, int a[][SIZE], int size)
+{
+    const char* ptr;
     int hold, pass, i;
     for (pass = 1; pass < size; ++pass) {
 
-        // loop to control number of comparisons per pass   
+           
         for (i = 0; i < size - pass; ++i) {
 
-            // compare adjacent elements and swap them if first 
-            // element is greater than second element           
+            
             if (a[i][3] < a[i + 1][3]) {
-                ptr=name[i];
-                name[i]=name[i+1];
-                name[i+1]=ptr;
+                ptr = name[i];
+                name[i] = name[i + 1];
+                name[i + 1] = ptr;
                 swap(&a[i][0], &a[i + 1][0]);
                 swap(&a[i][1], &a[i + 1][1]);
                 swap(&a[i][2], &a[i + 1][2]);
                 swap(&a[i][3], &a[i + 1][3]);
             }
         }
-    }
-
-    printf("\n\nLowest grade: %d\nHighest grade:%d\n",
-        minimum(a, stu, SIZE),
-        maximum(a, stu, SIZE));
-
-    for (size_t student = 0; student < stu; ++student) {
-        printf("The average grade for student %u is %.2f\n",
-            student, average(a[student], SIZE));
     }
 }
 
@@ -117,43 +102,4 @@ void swap(int* e1Ptr, int* e2Ptr)
     hold = *e1Ptr;
     *e1Ptr = *e2Ptr;
     *e2Ptr = hold;
-}
-int minimum(const int grades[][SIZE], size_t pupils, size_t tests)
-{
-    int lowGrade = 100;
-
-    for (size_t i = 0; i < pupils; ++i) {
-
-        for (size_t j = 0; j < tests; ++j) {
-
-            if (grades[i][j] < lowGrade) {
-                lowGrade = grades[i][j];
-            }
-        }
-    }
-    return lowGrade;
-}
-int maximum(const int grades[][SIZE], size_t pupils, size_t tests)
-{
-    int highGrade = 0;
-
-    for (size_t i = 0; i < pupils; ++i) {
-
-        for (size_t j = 0; j < tests; ++j) {
-
-            if (grades[i][j] > highGrade) {
-                highGrade = grades[i][j];
-            }
-        }
-    }
-    return highGrade;
-}
-double average(const int setOfGrades[], size_t tests)
-{
-    int total = 0;
-
-    for (size_t i = 0; i < tests; ++i) {
-        total += setOfGrades[i];
-    }
-    return (double)total / tests;
 }
